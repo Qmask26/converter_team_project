a51f8a6a8c082a802048a199297de2579ec7d287
boomb0om <igorekandco1@gmail.com>
2022-10-15 00:28:27 +0300

    Add antimirov derivative

--- src/derivatives/brzozovski.lua
+++ src/derivatives/brzozovski.lua
@@ -1,34 +1,6 @@
 local Regexs = require("src/model/regex")
+require "src/derivatives/utils"
 
-
-function new_regexnode(value, type, nchildren, firstChild, secondChild)
-	local regexnode = Regexs.RegexNode:new(value, false)
-	regexnode.type = type
-	regexnode.nchildren = nchildren
-	regexnode.firstChild = firstChild
-	regexnode.secondChild = secondChild
-	return regexnode
-end
-
-function node_is_empty_string(regex_node)
-	return regex_node.type == Regexs.operations.symbol and regex_node.value == ''
-end
-
-function check_if_epsilon_in_regex(regex_node)
-	if (regex_node.type == Regexs.operations.iter) then
-		return true
-	elseif (regex_node.type == Regexs.operations.alt) then
-		if node_is_empty_string(regex_node.firstChild) then
-			return true
-		elseif node_is_empty_string(regex_node.secondChild) then
-			return true
-		else
-			return check_if_epsilon_in_regex(regex_node.firstChild) or check_if_epsilon_in_regex(regex_node.secondChild)
-		end
-	end
-
-	return false
-end
 
 function simplify_outer(regex_node)
 	if regex_node.type == Regexs.operations.concat then
--- tests/regex.lua
+++ tests/regex.lua
@@ -1,19 +1,25 @@
 local Regexs = require("src/model/regex")
 require "src/derivatives/brzozovski"
+require "src/derivatives/antimirov"
 
 --local r = "a|b"
 --local regex_parsed = Regexs.RegexNode:new(r, true)
 --print(regex_parsed.value_for_print, regex_parsed.nchildren)
 
 print("Test epsilon in regex check")
+
 local r = Regexs.RegexNode:new("a|b", true)
 assert(check_if_epsilon_in_regex(r) == false)
+
 local r = Regexs.RegexNode:new("(a|b)*", true)
 assert(check_if_epsilon_in_regex(r) == true)
+
 local r = Regexs.RegexNode:new("a(bc)*d|b", true)
 assert(check_if_epsilon_in_regex(r) == false)
+
 local r = Regexs.RegexNode:new("a|(bc)*", true)
 assert(check_if_epsilon_in_regex(r) == true)
+
 local r = Regexs.RegexNode:new("((a|(c|d)*)|bc)", true)
 assert(check_if_epsilon_in_regex(r) == true)
 
@@ -25,10 +31,34 @@ local r = Regexs.RegexNode:new("a", true)
 local deriv = brzozovski_derivative("a", r)
 assert(deriv.value == "")
 
-local r = Regexs.RegexNode:new("a|b", true)
+local r = Regexs.RegexNode:new("a|ba", true)
 local deriv = brzozovski_derivative("a", r)
 assert(deriv.value_for_print == "_epsilon_")
 
 local r = Regexs.RegexNode:new("aa|a(b)*", true)
 local deriv = brzozovski_derivative("a", r)
 assert(deriv.value == "a|(b)*")
+
+--
+
+print("Test antimirov derivatives")
+
+function printv(v)
+	print(v)
+end
+
+local r = Regexs.RegexNode:new("ab", true)
+local deriv = antimirov_derivative("a", r)
+assert(deriv:str() == "b")
+
+local r = Regexs.RegexNode:new("a|ab", true)
+local deriv = antimirov_derivative("a", r)
+assert(deriv:str() == " b" or deriv:str() == "b ") -- "epsilon" union "b"
+
+local r = Regexs.RegexNode:new("aa|a(b)*", true)
+local deriv = antimirov_derivative("a", r)
+assert(deriv:str() == "a (b)*" or deriv:str() == "(b)* a")
+
+local r = Regexs.RegexNode:new("a|(b)*a", true)
+local deriv = antimirov_derivative("a", r)
+assert(deriv:str() == "") -- equals "epsilon"
