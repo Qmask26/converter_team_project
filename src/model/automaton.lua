ade5c62a70813df855b809fb039a6a289a7bc65e
Yuri Proskurnikov <qmask26@yandex.ru>
2022-10-16 16:48:26 +0300

    automaton

--- src/model/automaton.lua
+++ src/model/automaton.lua
@@ -1,25 +1,39 @@
-local class = require("middleclass")
+local class = require("src/model/middleclass")
 
 Automaton_module = {}
 
 Automaton = class("Automaton")
 Edge = class("Edge")
 State = class("State")
-Transition = class("Transition")
+
+function Automaton:initialize(states)
+    self.states = states
+end
 
 function Edge:initialize(symbol, label)
     self.symbol = symbol
-    self.label = label
+    self.lable = label
 end
 
-function State:initialize(index, finality, transitions)
-    self.index = index
-    self.finality = finality
-    self.transitions = transitions
+function Edge:annote(label)
+    self.lable = label
 end
 
-function Transition:initialize(to, edge)
-    self.transition = {[edge] = to}
+function Edge:deannote()
+    self.lable = nil
 end
+
+function State:initialize(isFinal, transitions)
+    self.isFinal = isFinal --bool
+    self.transitions = transitions -- пары ребро-индекс состояния
+end
+
+function State:transit(edge)
+    return self.transitions[edge]
+end
+
+Automaton_module.Automaton = Automaton
+Automaton_module.State = State
+Automaton_module.Edge = Edge
 
 return Automaton_module
