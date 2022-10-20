63e82ec5be3f98a32f6923b237839cdd1e414813
boomb0om <igorekandco1@gmail.com>
2022-10-20 17:31:27 +0300

    Update pumplength test

--- tests/pumplength.lua
+++ tests/pumplength.lua
@@ -1,11 +1,19 @@
 local Regexs = require("src/model/regex")
 require "src/derivatives/brzozovski"
+require("src/r2nfa_converter/thompson")
 
+local regex_s = Regexs.Regex:new("(a|b*)")
+local nfa = create_thompson_automaton(regex_s)
 
-function check_if_infix_pumps()
-	print()
+for ind_from, table_symbols in pairs(nfa.transitions) do
+	for symbol, table_labels in pairs(table_symbols) do
+		for i, to in pairs(table_labels[""]) do
+			local is_final = nfa.finality[to]
+			print(ind_from, symbol, to, is_final)
+		end
+	end
 end
-
+--[[
 local regex_s = "regex"
 local rtree = Regexs.RegexNode:new(regex_s, true)
 
@@ -13,7 +21,8 @@ local i, j
 local n = 1
 while n >= 1 do
 	local prefix = string.sub(regex_s, 1, n) 
-	local deriv = 1
+	local deriv = brzozovski_derivative_word(prefix, rtree)
+	print(prefix, deriv.value)
 
 	for i = 1, #prefix do
 		for j = i, #prefix do
@@ -28,3 +37,4 @@ while n >= 1 do
 		break
 	end
 end
+--]]
