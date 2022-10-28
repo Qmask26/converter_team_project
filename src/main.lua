b5c277aa8d752cb675016072f5cb93da86a048bb
Yuri Proskurnikov <qmask26@yandex.ru>
2022-10-29 02:00:16 +0300

    Dynamic typecheck and fixes

--- src/main.lua
+++ src/main.lua
@@ -2,21 +2,18 @@ if (#arg == 1 or arg[1] ~= "-d" and arg[1] ~= "-s") then
     print("Incorrect, try to: 'lua main.lua [-s | -d] [path/to/input/file]'")
     print(#arg)
     return
-elseif (arg[1] == "-s") then
-    typechecker = require("src/typecheck/static")
-elseif (arg[1] == "-d") then
-    typechecker = require("src/typecheck/dynamic")
 end
-parser = require("src/utils/input_parser")
 
-typechecker:typecheck(arg[2])
-expressions = parser:parse(arg[2])
-print(#expressions)
-for _, v in pairs(expressions) do
+Parser = require("src/utils/input_parser")
+
+local parser = Parser:new(arg[1] == "-s")
+
+local expressions = parser:parse(arg[2])
+
+for k, v in pairs(expressions) do
     if (v == nil) then
-        print("nil")
-    else
-        print("Computing", v.value.name)
+        print("ERROR")
+    else 
         v.value:compute()
     end
 end
