1bf1bbd2817de97f04550934cc4cae60d6e95cf4
Gervva <germanvladimr@yandex.ru>
2022-10-30 18:38:19 +0300

    Test Subset

--- tests/intersection.lua
+++ tests/intersection.lua
@@ -7,17 +7,20 @@ require "src/automaton_functions/intersection"
 require("src/automaton_functions/minimization")
 
 local r1 = Regex.Regex:new("(a|ba)*")
-local nfa = create_thompson_automaton(r1)
-local dfa1 = Det(nfa)
+local nfa1 = create_thompson_automaton(r1)
+local dfa1 = Det(nfa1)
 dfa1:addTrap()
 print(dfa1:tostring())
 
 local r2 = Regex.Regex:new("(ba)*")
-local nfa = create_thompson_automaton(r2)
-local dfa2 = Det(nfa)
+local nfa2 = create_thompson_automaton(r2)
+local dfa2 = Det(nfa2)
 dfa2:addTrap()
 print(minimization(dfa2):tostring())
 
-local dfa_intersect = minimization(intersect_dfa(dfa1, dfa2))
-print(minimization(dfa_intersect):tostring())
-print(EquivNFA(dfa2, dfa_intersect))
+print(tostring(SubsetNFA(nfa2, nfa1)))
+print(tostring(SubsetRegex(r2, r1)))
+
+--local dfa_intersect = minimization(intersect_dfa(dfa1, dfa2))
+--print(minimization(dfa_intersect):tostring())
+--print(EquivNFA(dfa2, dfa_intersect))
