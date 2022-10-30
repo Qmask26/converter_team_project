af46f3ae7688123df0c49971695532b7322d7d51
boomb0om <igorekandco1@gmail.com>
2022-10-30 14:23:14 +0300

    Add subset test

--- tests/subset.lua
+++ tests/subset.lua
@@ -3,6 +3,6 @@ require "src/utils/common"
 require "src/predicates/predicates"
 
 local r1 = Regexs.Regex:new("(a|ba)*")
-local r2 = Regexs.Regex:new("(ba|bb)")
+local r2 = Regexs.Regex:new("(ba)")
 
 print(SubsetRegex(r1, r2))
