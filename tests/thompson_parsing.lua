3c22f85f1faa2f8112e49cf1fe6d3fb20137fa59
artem-yadr <ciceronazaz2002@gmail.com>
2023-01-14 10:08:40 +0300

    -thompson algorithm added

--- tests/thompson_parsing.lua
+++ tests/thompson_parsing.lua
@@ -2,14 +2,13 @@ local Automaton = require("src/model/automaton")
 local Regexs = require("src/model/regex")
 require("src/functions/thompson_parsing")
 
-local statesNumber = 5
-local finalStates = {5}
+local statesNumber = 2
+local finalStates = {1, 2}
 local transitions = {
-    {from = 1, symbol = "a", to = 2, label = ""},
-    {from =1, symbol = 'a', to = 3, label = ""},
-    {from = 1, symbol = "_epsilon_", to = 4, label = ""},
-    {from = 4, symbol = 'a', to = 5, label = ""}
+    {from = 1, symbol = "a", to = 1, label = ""},
+    {from =1, symbol = 'a', to = 2, label = ""},
+    {from = 2, symbol = "b", to = 2, label = ""}
 }
 local nfa = Automaton.Automaton:new(statesNumber, finalStates, transitions, false, {1})
 
-thompson_parsing(nfa, 'a')
+print(thompson_parsing(nfa , "aaaabbba"))
